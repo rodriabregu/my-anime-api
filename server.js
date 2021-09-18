80bee4e5e386482156a6e22b84578131b6769189
Rodrigo Alcino <rodrigo_alcino@hotmail.com>
2021-09-18 20:32:57 -0300

    allowed request

--- server.js
+++ server.js
@@ -6,5 +6,18 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        res.status(403).send('Not allowed, sorry my friend.')
+    }
+    if (req.method === 'PUT') {
+        res.status(403).send('Not allowed, sorry my friend.')
+    }
+    if (req.method === 'DELETE') {
+        res.status(403).send('Not allowed, sorry my friend.')
+    }
+    // Continue to JSON Server router
+    next()
+  })
 
-server.listen(port, console.log('Server On'));
+server.listen(port);
